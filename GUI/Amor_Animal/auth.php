3e9a5de2b8ba7cbe043016814f1c702001296e36
asoto134 <sotoadrian4@gmail.com>
2015-04-04 18:07:37 -0600

    Modificaciones en nombres de variables

--- GUI/Amor_Animal/auth.php
+++ GUI/Amor_Animal/auth.php
@@ -1,33 +1,32 @@
 <?php
 include ("settings.php");
 include ("common.php");
-if(isset($_COOKIE['casaculturalamon_id']))
+if(isset($_COOKIE['id']))
 {
-   $conex=mysql_connect(HOST,USER,PASS);
-   @mysql_select_db(DB,$conex) or die("Error en la seleccion, '$php_errormsg'");
-   $username = $_COOKIE['casaculturalamon_id'];
-   $pass = $_COOKIE['casaculturalamon_key'];
-   $administrador = $_COOKIE['casaculturalamon_admin'];
-   $check = mysql_query("SELECT * FROM Funcionario WHERE nick = '$username'")or die(mysql_error());
-   mysql_close();
-   while($info = mysql_fetch_array( $check ))
+   $username = $_COOKIE['id'];
+   $pass = $_COOKIE['key'];
+   $administrador = $_COOKIE['admin'];
+   
+   $conn = oci_connect(USER, PASS, HOST);
+   $stmt = oci_parse($conn, "SELECT * FROM USUARIO WHERE NOM_USUARIO = '" . $username . "'" );
+   oci_execute($stmt);
+   while(($row = oci_fetch_array($stmt)))
    {
-      if ($pass != $info['password']){
-         header("Location: index.php");
-         //print "Error de contraseña";
-      }
-      else
-      {
-         actualizar_cookie($username,$pass,$administrador);
-      }
+       
+        if ($pass != $row['CONTRASENIA'])
+        {
+            header("Location: index.php");
+        }
+        else
+        {
+            actualizar_cookie($username,$pass,$administrador);
+        }
    }
+   oci_close($conn);
 }
 else
 
 {
-//   print "No esta conectado";
-//   print "\n <a href=index.php>Ingresar</a>";
-//   sleep(10);
    header("Location: index.php");
 }
 ?>
