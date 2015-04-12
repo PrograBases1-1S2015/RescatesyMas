07b17aabca44f5bf91cdc041deff0d06e7fcc366
Amanda <sofia.29@hotmail.es>
2015-04-11 18:08:39 -0600

    Obtener id de adoptante o rescatista
    Función que devuelve un id de la tabla de rescatista o adoptante con tan
    solo el nombre del usuario

---
+++ ScriptProgra/18.Obtener id de usuario o rescatista.sql
@@ -0,0 +1,50 @@
+--Funcion para buscar un adoptante--
+create or replace function Buscar_Adoptante(NUsuario integer) return integer is
+  Result integer;
+begin
+  Select adp.adoptante_id into result
+  from adoptante adp
+  inner join persona per
+  on per.persona_id=adp.persona_id and per.persona_id=NUsuario;
+  return(Result);
+end Buscar_Adoptante;
+--Función para buscar un rescatista--
+create or replace function Buscar_Rescatista(NUsuario number) return number is
+    Result integer;
+begin
+ Select res.rescatista_id into result
+  from rescatista res
+  inner join persona per
+  on per.persona_id=res.rescatista_id and per.persona_id=NUsuario;
+  return(Result);
+end Buscar_Rescatista;
+--Función que retornar el id ya sea de un rescatista o de un adoptante--
+create or replace function ID_Rescatista_Adoptante(NUsuario Varchar) return number is
+  Result number;
+  IDUsuario integer;
+  IDPersona integer;
+  Existe integer;
+
+begin
+  Select usuario_id(NUsuario) into IDUsuario from dual;
+  Select persona_id(IDUsuario)into IDPersona from dual;
+
+  --Mediante un count se búscara en la tabla de adoptantes si el
+  --registro existe. Si manda un uno es que está y por lo tanto almacenará
+  --devolver el dato.
+
+  Select count(1) into existe
+  from dual
+  where exists(
+        select 1
+        from Adoptante
+        where adoptante.persona_id=IDPersona
+      );
+
+   if existe = 1 then
+      Select Buscar_Adoptante(IDPersona) into Result from dual;
+    else
+     Select Buscar_Rescatista(IDPersona) into Result from dual;
+   end if;
+  return(Result);
+end ID_Rescatista_Adoptante;
