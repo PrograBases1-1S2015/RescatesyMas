049cee6386e82f7c16041897de5254d7df7991fb
Amanda <sofia.29@hotmail.es>
2015-04-27 22:32:22 -0600

    Devoluciones y adopciones listas
    Adopta y devuelve mascotas.
    Cuando se duelven se borran de la tabla de adopcionesxmascota, ya que en
    base a esta tabla se carga el combox de las mascotas

--- GUI/Amor_Animal/js/misFunciones.js
+++ GUI/Amor_Animal/js/misFunciones.js
@@ -255,3 +255,64 @@ function justNumbers(e)
  
         return /\d/.test(String.fromCharCode(keynum));
     }
+//Agregar a misfunciones.js//
+/////////////Funciones para el administrador////////
+//No es la mejor forma de hacerlo, pero es la única que se me ocurre
+function getProvincia1(id_ComboPais)
+{ 
+    
+    xmlhttp = GetXmlHttpObject();
+
+    if (xmlhttp == null)
+    {
+            alert ("El navegador no soporta la solicitud HTTP realizada.");
+            return;
+    }
+    xmlhttp.onreadystatechange = stateChangedProvincia1;
+    xmlhttp.open("POST","buscarProvincias.php",true);
+
+//establece el header para la respuesta
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+//enviamos las variables al archivo get_combo2.php
+    xmlhttp.send("q=" + id_ComboPais);	
+
+}
+
+function stateChangedProvincia1()
+{
+    if (xmlhttp.readyState===4 && xmlhttp.status===200)
+    {
+        document.getElementById("com_Provincia1").innerHTML=xmlhttp.responseText;
+    }
+}
+////////////////////////////////////////////////////////////////////////////////7
+function get_Tratamiento(id_ComboEnfermedad)
+{ 
+    
+    xmlhttp = GetXmlHttpObject();
+
+    if (xmlhttp == null)
+    {
+            alert ("El navegador no soporta la solicitud HTTP realizada.");
+            return;
+    }
+    xmlhttp.onreadystatechange = stateChangedTratamiento;
+    xmlhttp.open("POST","buscartratamiento.php",true);
+
+//establece el header para la respuesta
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+//enviamos las variables al archivo get_combo2.php
+    xmlhttp.send("q=" + id_ComboEnfermedad);	
+
+}
+
+function stateChangedTratamiento()
+{
+    if (xmlhttp.readyState===4 && xmlhttp.status===200)
+    {
+        document.getElementById("com_Tratamiento").innerHTML=xmlhttp.responseText;
+    }
+}
+
--- ScriptProgra/19.Devoluciones y Adopciones.sql
+++ ScriptProgra/19.Devoluciones y Adopciones.sql
@@ -1,7 +1,10 @@
-create or replace procedure Agregar_Devoluciones(adoptante number,mascota number,fecha date,causa number) is
+create or replace procedure Agregar_Devoluciones(adoptante varchar2,mascota number,causa number) is
+       IDA number;
 begin
+  select buscar_adoptante(adoptante) into IDA from dual;
   insert into devoluciones_x_mascota(devoluciones_x_mascota_id,adoptante_id,mascota_id,fecha,causa_id)
-  values(incremento_devo_x_mascota.nextval,adoptante,mascota,fecha,causa);
+  values(incremento_devo_x_mascota.nextval,IDA,mascota,SYSDATE,causa);
+  
   update mascota 
   set estado_mascota_id = 1
   where mascota_id = mascota;
