54d52b8beedfe8860d093ac72a39b3512f37af0e
nelss95 <nelsk895@gmail.com>
2015-04-27 23:44:58 -0600

    #

--- ScriptProgra/21.Buscar_Mascota.sql
+++ ScriptProgra/21.Buscar_Mascota.sql
@@ -1,5 +1,5 @@
 --Búsqueda de mascota global--
-create or replace procedure Buscar_Mascota(nombre_mascota varchar2,distrito_busqueda varchar2,raza_mascota number,color_mascota number,estado varchar2,tamanio_mascota number,
+create or replace procedure Buscar_Mascota(nombre_mascota varchar2,distrito_busqueda varchar2,raza_mascota number,color_mascota number,estado number,tamanio_mascota number,
 tipo_mascota number, nivel_energia_mascota number,entrenamiento_mascota number,Result out sys_refcursor) is
 begin  
   --Abrir cursor para guardar toda la información--
@@ -18,5 +18,7 @@ inner join tamanio t on m.tamanio_id = t.tamanio_id
 inner join facilidad_entrenamiento fe on m.facilidad_entrenamiento_id = fe.facilidad_entrenamiento_id
 inner join rescatista re on m.rescatista_id = re.rescatista_id
 inner join persona p on re.persona_id = p.persona_id
+where UPPER(m.nombre) like UPPER(nombre_mascota) or UPPER(d.distrito) like (distrito_busqueda) or 
+m.raza_id = raza_mascota or m.color_id = color_mascota or m.estado_mascota_id = estado or m.tamanio_id = tamanio_mascota or
+m.nivel_energia_id=nivel_energia_mascota or m.facilidad_entrenamiento_id=entrenamiento_mascota;
 end Buscar_Mascota;
-    
