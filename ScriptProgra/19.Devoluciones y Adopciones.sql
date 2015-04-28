c21266fc4242705e7d481838042b99d05d31daa0
Amanda <sofia.29@hotmail.es>
2015-04-27 22:38:03 -0600

    Cambio en adopciones
    Se me olvido guardar el cambio

--- ScriptProgra/19.Devoluciones y Adopciones.sql
+++ ScriptProgra/19.Devoluciones y Adopciones.sql
@@ -11,12 +11,20 @@ begin
 end Agregar_Devoluciones;
 
 
-create or replace procedure Agregar_Adopciones(adoptante number, mascota number, foto blob) is
+create or replace procedure Agregar_Adopciones(Usuario varchar2,IDM number) is
+   ID_USUARIO NUMBER;
+   ID_ADOPTANTE NUMBER;
 begin
-  insert into adopciones_x_mascota(adopciones_x_mascota_id,adoptante_id,mascota_id,foto)
-  values(incremento_adop_x_mascota.nextval,adoptante,mascota,foto);
-  update mascota
-  set estado_mascota_id = 2
-  where mascota_id = mascota;
+  SELECT USUARIO_ID(Usuario) INTO ID_USUARIO FROM DUAL;
+  SELECT BUSCAR_ADOPTANTE(ID_USUARIO) INTO ID_ADOPTANTE FROM DUAL;
+
+  INSERT INTO ADOPCIONES_X_MASCOTA(ADOPCIONES_X_MASCOTA_ID,ADOPTANTE_ID,MASCOTA_ID)
+  VALUES (INCREMENTO_ADOP_X_MASCOTA.NEXTVAL,ID_ADOPTANTE,IDM);
+
+  UPDATE MASCOTA MAS
+  SET MAS.ESTADO_MASCOTA_ID=2
+  WHERE MAS.MASCOTA_ID=IDM;
+
+
 end Agregar_Adopciones;
 
